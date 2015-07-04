7a381fb289973ae28fc0629c6be58301626aca64
Scott <scsilver.umd@gmil.com>
2015-07-03 20:33:41 -0600

    improve factory for mood-analyzer test

--- spec/factories/entries.rb
+++ spec/factories/entries.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do 
 	factory :entry do |f| 
 	f.title "My Life" 
-	f.content "Happy Happy Sad Big" 
+	f.content "Happy Happy Sad Glad Small tiny kill Big" 
 	f.user_id "1"
 	end 
 end
