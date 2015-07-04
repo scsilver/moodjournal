cebe579b0bfd0e8806e0b803f70f983fcb7ac356
Scott <scsilver.umd@gmil.com>
2015-07-03 19:33:12 -0600

    rspec units for mood-analyzer

--- spec/models/mood_analyzer_spec.rb
+++ spec/models/mood_analyzer_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe MoodAnalyzer do
+	
   	it "has a valid factory" do
   		FactoryGirl.create(:entry).should be_valid
   	end
@@ -10,4 +11,37 @@ describe MoodAnalyzer do
 			MoodAnalyzer.new(entry).analyze.should == 1
 			end
 		end
+
+		describe "#moodsign" do
+			it "determines if content is positive negative or neither when sum is 1" do
+				sum = 1
+				entry = FactoryGirl.build(:entry)
+				MoodAnalyzer.new(entry).moodsign(sum)
+				entry.positive.should == true
+				entry.negative.should == false  
+				end
+			it "determies if content is positive negative or neither when sum is -1" do
+				sum = -1
+				entry = FactoryGirl.build(:entry)
+				MoodAnalyzer.new(entry).moodsign(sum)
+				entry.positive.should == false
+				entry.negative.should == true  
+				end
+			it "determies if content is positive negative or neither when sum is 0" do
+				sum = 0
+				entry = FactoryGirl.build(:entry)
+				MoodAnalyzer.new(entry).moodsign(sum)
+				entry.positive.should == false
+				entry.negative.should == false  
+				end
+			end
+		describe "#moodintensity" do
+			it "finds distance of sum from zero" do
+				sum = 1
+				entry = FactoryGirl.build(:entry)
+				MoodAnalyzer.new(entry).moodintensity(sum)
+				entry.intensity.should == 1
+			end
+		end
+
 end
