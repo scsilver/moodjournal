4f902c13f4804628d474ea5c272f6b5fe383da45
Scsilver <scsilver.umd@gmail.com>
2016-01-15 03:56:55 -0700

    entries controller changes

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -5,7 +5,7 @@ class EntriesController < ApplicationController
   # GET /entries.json
 
 
-def index
+  def index
 
 
     if params[:query].present?
@@ -14,6 +14,7 @@ def index
     else
     @entries = Entry.all
     end
+  
   end
 
 
